d18b10f2b88591927e333c9af5439a33cc61228e
DevAles <devalesoficial@gmail.com name = DevAles>
2023-07-06 16:27:36 -0300

    Add rust to path and add improve installation via build script

--- scripts/distrotunnel.zsh
+++
@@ -1,8 +0,0 @@
-export DISTROTUNNEL=~/.local/share/distrobox/distrotunnel;
-export PATH=$DISTROTUNNEL/bin:$PATH;
-
-alias distrotunnel="distrobox enter distrotunnel"
-alias distrotunnel_setup=$DISTROTUNNEL/scripts/setup
-alias distrotunnel_unsetup=$DISTROTUNNEL/scripts/unsetup
-
-source $DISTROTUNNEL/scripts/dmt/src/dmt.zsh
